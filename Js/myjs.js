d61cce4ec859f0d68869f9123f3ac505252625fb
York Chan <346769090@qq.com>
2016-01-24 15:44:01 +0800

    cc

--- Js/myjs.js
+++ Js/myjs.js
@@ -1,13 +1,11 @@
-(function ($) {resizeBannerImage();
-	window.onresize=resizeBannerImage;//当窗口改变宽度时执行此函数
+(function ($) {
   $(function () {
     // Checkboxes and Radio buttons
-    $('[data-toggle="checkbox"]').radiocheck();
     $('[data-toggle="radio"]').radiocheck();
-	
-
+	resizeBannerImage();
+	window.onresize=resizeBannerImage;//当窗口改变宽度时执行此函数
   });
-function resizeBannerImage()
+function resizeBannerImage()//riadio按钮排列函数
    {
     if( $(window).width() > 800 ) {
      $(".radiobox").removeClass("col-xs-6");
